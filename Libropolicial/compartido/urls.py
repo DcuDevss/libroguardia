c158b0f86f12fd9336dca09eabc337248f0f2f2c
gabriel <102371072+Jgmdevelopers@users.noreply.github.com>
2025-01-03 13:54:10 -0300

    vistas para el reset en login del pass

--- Libropolicial/compartido/urls.py
+++ Libropolicial/compartido/urls.py
@@ -10,8 +10,15 @@ urlpatterns = [
     path('no-permission/', no_permission, name='no_permission'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # Añadido el LogoutView
    # path('check-message/', check_message, name='check_message'),
+
     path('perfil/', perfil_usuario, name='perfil_usuario'),
     path('password_change/', cambiar_contrasena, name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
     
 ]
