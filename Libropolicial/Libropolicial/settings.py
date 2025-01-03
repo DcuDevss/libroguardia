8edf3ac79dfb62df57a595738df40f79be87a34a
gabriel <102371072+Jgmdevelopers@users.noreply.github.com>
2025-01-03 16:02:51 -0300

    processo de reestablecimiento de password

--- Libropolicial/Libropolicial/settings.py
+++ Libropolicial/Libropolicial/settings.py
@@ -215,3 +215,12 @@ CKEDITOR_CONFIGS = {
         'font_names': 'Arial/Arial, Helvetica, sans-serif; Times New Roman/Times New Roman, Times, serif; Verdana/Verdana, Geneva, sans-serif; Courier New/Courier New, Courier, monospace',
     },
 }
+
+# Configuración de correo electrónico para Django
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Servidor SMTP de Gmail
+EMAIL_PORT = 587               # Puerto para TLS
+EMAIL_USE_TLS = True           # Habilitar TLS
+EMAIL_HOST_USER = 'gabrielmolinalp@gmail.com'  # Tu correo electrónico
+EMAIL_HOST_PASSWORD = 'xuoceirxnhgpwkta'       # Contraseña generada de la app de Gmail
+DEFAULT_FROM_EMAIL = 'turnos.dicu@dcu.sistemas.com'  # Dirección del remitente
