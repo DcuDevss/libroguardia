f8a156fa0fdae378377e941fbb383d03f8eee49e
gabriel <102371072+Jgmdevelopers@users.noreply.github.com>
2025-01-03 15:02:49 -0300

    arreglo en la descarga del pdf luego de la advertencia de registros activos

--- Libropolicial/compartido/utils.py
+++ Libropolicial/compartido/utils.py
@@ -5,7 +5,7 @@ def user_is_in_group(user, group_name):
 
 from django.http import JsonResponse
 
-def verificar_registros_activos(model, estado_field='estado', activo_field='activo', estado_valor=True, activo_valor=True):
+def verificar_registros_activos(request, model, estado_field='estado', activo_field='activo', estado_valor=True, activo_valor=True):
     """
     Verifica si hay registros activos en el modelo dado.
 
@@ -16,12 +16,18 @@ def verificar_registros_activos(model, estado_field='estado', activo_field='acti
     :param activo_valor: El valor esperado del campo de activo para considerarlo activo.
     :return: JsonResponse si hay registros activos, None si no hay problemas.
     """
+    # Verifica si se debe forzar la descarga
+    force_download = request.GET.get('force', 'false').lower() == 'true'
+
     # Filtra registros que cumplan con los criterios de estado y activo.
     filtro = {estado_field: estado_valor, activo_field: activo_valor}
     registros_activos = model.objects.filter(**filtro).exists()
 
-    if registros_activos:
-        # Retornar una respuesta JSON indicando que hay registros activos
-        return JsonResponse({'warning': f'Existen registros con el estado "Activo" en {model._meta.verbose_name}. ¿Deseas continuar con la descarga del PDF?'})
-
+    if registros_activos and not force_download:
+        # Si la solicitud es AJAX, retorna un JSON con advertencia
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return JsonResponse({
+                'warning': f'Existen registros con el estado "Activo" en {model._meta.verbose_name}. ¿Deseas continuar con la descarga del PDF?'
+            })
+        
     return None
