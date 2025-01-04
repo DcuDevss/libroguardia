ec834e3f76af8a6b3bb82a3e3f2697db1fb26f52
gabriel <102371072+Jgmdevelopers@users.noreply.github.com>
2025-01-04 11:48:23 -0300

    comente solo la funcion en el views

--- Libropolicial/compartido/urls.py
+++ Libropolicial/compartido/urls.py
@@ -11,10 +11,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # Añadido el LogoutView
    # path('check-message/', check_message, name='check_message'),
 
+    # rutas el cambio de contraseña desde la vista de perfil
     path('perfil/', perfil_usuario, name='perfil_usuario'),
     path('password_change/', cambiar_contrasena, name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
 
+    # rutas el reestablecimiento de contraseña
     path('password_reset/', CustomPasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
