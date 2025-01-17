cd8f169356dd904f65a869e648ab6d5d2ccc1574
Cristian <retacris30@gmail.com>
2025-01-17 11:41:23 -0300

    comit desde server

--- Libropolicial/compartido/views.py
+++ Libropolicial/compartido/views.py
@@ -49,7 +49,7 @@ class CustomLoginView(LoginView):
             'comisaria_terceraRG': 'comisariaterceraRG_list',
             'comisaria_cuartaRG': 'comisariacuartaRG_list',
             'comisaria_quintaRG': 'comisariaquintaRG_list',
-            #'comisaria_tolhuin': 'comisaria_tolhuin_list',
+            #'comisaria_tolhuin': 'comisaria_tolhuin_list', esta comentado por el modemnto
             #'divisioncomunicaciones': 'divisioncomunicaciones_list'
         }
         for group, url in user_group_redirects.items():
